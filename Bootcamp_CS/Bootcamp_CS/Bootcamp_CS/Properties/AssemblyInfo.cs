3fd4cf13930fea36d0dc1bc475b5955ab1234be7
StefanCroes <stefan.croes@polteq.com>
2017-12-28 11:12:12 +0100

    csharp added

---
+++ Bootcamp_CS/Bootcamp_CS/Bootcamp_CS/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bootcamp_CS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bootcamp_CS")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e78d4f1a-f780-4d4e-aa9e-3d2090264ad2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Bootcamp_CS/Bootcamp_CS/Bootcamp_CS/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace BootCamp
+{
+    [TestClass]
+    public class Inloggen
+    {
+        static IWebDriver driverGC;
+
+        [AssemblyInitialize]
+        public static void SetUp(TestContext context)
+        {
+            driverGC = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void Login()
+        {
+            driverGC.Navigate().GoToUrl("https://techblog.polteq.com/testshop/index.php");
+            driverGC.FindElement(By.ClassName("login")).Click();
+            driverGC.FindElement(By.Id("email")).SendKeys("stefan.croes@polteq.com");
+            driverGC.FindElement(By.Id("passwd")).SendKeys("Z9E/Kcvw");
+            driverGC.FindElement(By.Id("SubmitLogin")).Click();
+            IWebElement webElement = driverGC.FindElement(By.ClassName("account"));
+            Assert.AreEqual(webElement.Text, "Stefan Croes");
+            driverGC.Close();
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
+
